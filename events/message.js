e64e29ea5214e0c98d15c49aca48d5213b408f3e
MrMcEpic <45011453+MrMcEpic@users.noreply.github.com>
2019-05-15 16:51:39 -0400

    Fixed some typos and optimized readability

--- events/message.js
+++ events/message.js
@@ -1,82 +1,54 @@
 const addrole = require('../commands/addrole')
 module.exports = (client, message) => {
   if (message.channel.name === 'introduction') {
-    let list = []
+
+    //for roles that have one easy spelling
+    const simpleRoles = ["Macaw", "Cockatoo", "Eclectus", "Amazon", "Alexandrine", "Caique", "Senegal", "Cockatiel", "Quaker", "Rosella", "Kakariki", "Parrolet", "Canary", "Finch"];
+    let listToAdd = []
     let words = message.content.toLowerCase();
-    if (words.search("budgie") >= 0 || words.search("parakeet") >= 0) {
-      list.push('Budgie')
+    for (const role of simpleRoles) {
+      if (words.includes(role.toLowerCase())) {
+        listToAdd.push(role);
+      }
     }
-    if (words.search("conure") >= 0) {
-      list.push('Conure')
+
+    //list of alt spellings
+
+    if (words.includes("budgie") || words.includes("parakeet")) {
+      listToAdd.push('Budgie')
     }
-    if (words.search("macaw") >= 0) {
-      list.push('Macaw')
+    if (words.includes("conure") || words.includes("gcc")) {
+      listToAdd.push('Conure')
     }
-    if (words.search("cockatoo") >= 0) {
-      list.push('Cockatoo')
+    if (words.includes("african grey") || words.includes("african gray") || words.includes("africangrey") || words.includes("africangray") || words.includes("african-grey") || words.includes("african-gray")) {
+      listToAdd.push('African Grey')
     }
-    if (words.search("eclectus") >= 0) {
-      list.push('Electus')
+    if (words.includes("lorikeet") || words.includes("lori")) {
+      listToAdd.push('Lorikeet')
     }
-    if (words.search("amazon") >= 0) {
-      list.push('Amazon')
+    if (words.includes("red-fan") || words.includes("red fan") || words.includes("redfan")) {
+      listToAdd.push('Red-fan Parrot')
     }
-    if (words.search("african grey") >= 0 || words.search("african gray") >= 0 || words.search("africangrey") >= 0 || words.search("africangray") >= 0 || words.search("african-grey") >= 0 || words.search("african-gray") >= 0) {
-      list.push('African Grey')
+    if (words.includes("barra")) {
+      listToAdd.push('Barraband')
     }
-    if (words.search("alexandrine") >= 0) {
-      list.push('Alexandrine')
+    if (words.includes("broto")) {
+      listToAdd.push('Brotogeris')
     }
-    if (words.search("lorikeet") >= 0) {
-      list.push('Lorikeet')
+    if (words.includes("lovebird") || words.includes("love bird") || words.includes("love-bird")) {
+      listToAdd.push('Lovebird')
     }
-    if (words.search("caique") >= 0) {
-      list.push('Caique')
+    if (words.includes("duck")) {
+      listToAdd.push('Ducks')
     }
-    if (words.search("red-fan") >= 0 || words.search("red fan") >= 0 || words.search("redfan") >= 0) {
-      list.push('Red-fan Parrot')
+    if (words.includes("ringneck") || words.includes("ring neck") || words.includes("ring-neck")) {
+      listToAdd.push('Ringneck')
     }
-    if (words.search("senegal") >= 0) {
-      list.push('Senegal')
-    }
-    if (words.search("barraband") >= 0) {
-      list.push('Barraband')
-    }
-    if (words.search("cockatiel") >= 0) {
-      list.push('Cockatiel')
-    }
-    if (words.search("brotogeris") >= 0) {
-      list.push('Brotogeris')
-    }
-    if (words.search("quaker") >= 0) {
-      list.push('Quaker')
-    }
-    if (words.search("rosella") >= 0) {
-      list.push('Rosella')
-    }
-    if (words.search("kakariki") >= 0) {
-      list.push('Kakariki')
-    }
-    if (words.search("lovebird") >= 0 || words.search("love bird") >= 0 || words.search("love-bird") >= 0) {
-      list.push('Lovebird')
-    }
-    if (words.search("parrolet") >= 0) {
-      list.push('Parrolet')
-    }
-    if (words.search("canary") >= 0) {
-      list.push('Canary')
-    }
-    if (words.search("finch") >= 0) {
-      list.push('Finch')
-    }
-    if (words.search("duck") >= 0) {
-      list.push('Ducks')
-    }
-    if (words.search("ringneck") >= 0 || words.search("ring neck") >= 0 || words.search("ring-neck") >= 0) {
-      list.push('Ringneck')
-    }
-    if (list.length > 0) {
-      return addrole(message, list)
+
+    //list end
+
+    if (listToAdd.length > 0) {
+      return addrole(message, listToAdd)
     } else {
       return addrole(message, ['Interested in birbs'])
     }
