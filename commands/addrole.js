4529b5706674139c7c1ceb6bcb0f98bc9627a799
MrMcEpic <45011453+MrMcEpic@users.noreply.github.com>
2019-05-25 12:45:04 -0400

    allows role updating

--- commands/addrole.js
+++ commands/addrole.js
@@ -1,4 +1,8 @@
 module.exports = (message, list) => {
+  if (message.member.roles.find(r => r.name === 'Interested in birbs') && list[0] != 'Interested in birbs') {
+    let role = message.guild.roles.find(r => r.name === 'Interested in birbs')
+    member.removeRole(role)
+  }
   for (i in list) {
     let member = message.member
     let role = message.guild.roles.find(r => r.name === list[i])
