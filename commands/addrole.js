d3753aa6209303b837eb6ed546c9c0ea2d6008ef
MrMcEpic <45011453+MrMcEpic@users.noreply.github.com>
2019-05-25 12:50:55 -0400

    fix

--- commands/addrole.js
+++ commands/addrole.js
@@ -1,10 +1,10 @@
 module.exports = (message, list) => {
+  let member = message.member
   if (message.member.roles.find(r => r.name === 'Interested in birbs') && list[0] != 'Interested in birbs') {
     let role = message.guild.roles.find(r => r.name === 'Interested in birbs')
     member.removeRole(role)
   }
   for (i in list) {
-    let member = message.member
     let role = message.guild.roles.find(r => r.name === list[i])
     member.addRole(role)
   }
