34b7e64280c137111d2537d7d303958f9e65fb49
MrMcEpic <45011453+MrMcEpic@users.noreply.github.com>
2019-06-23 15:05:10 -0400

    fix

--- commands/deleteandshare.js
+++ commands/deleteandshare.js
@@ -1,5 +1,4 @@
 module.exports = (client, message) => {
-  const member = message.member
   const channel = client.channels.get("374191834827718658")
-  channel.send(member.author.username)
+  channel.send(message.author.username)
 }
